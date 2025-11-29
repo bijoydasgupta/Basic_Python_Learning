4854637501267ea1bf8cad378ac92ab197fd7746
bijoydasgupta <bijoydasgupta595@gmail.com>
2025-11-30 00:33:44 +0600

    function

--- functions.py
+++ functions.py
@@ -7,8 +7,19 @@ greet()
 
 
 def greet2(first_name, last_name):
-    print(f"\nHi there {first_name} {last_name}!")
+    print(f"\nHi {first_name} {last_name}!")
     print("Welcome abord")
 
 
 greet2("Bijoy", "Das Gupta")
+greet2("Joy", "Das Gupta")
+
+
+def get_greeting(name):
+    return f"\n\nHi {name}"
+
+
+message = get_greeting("Bijoy")
+# print(message)
+file = open("content.txt", "w")
+file.write(message)
