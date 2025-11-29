74cd08372c0c68bf628679088d2bff36ab93fd11
bijoydasgupta <bijoydasgupta595@gmail.com>
2025-11-29 12:59:43 +0600

    def function

---
+++ functions.py
@@ -0,0 +1,14 @@
+def greet():
+    print("\nHi there !")
+    print("Welcome abord")
+
+
+greet()
+
+
+def greet2(first_name, last_name):
+    print(f"\nHi there {first_name} {last_name}!")
+    print("Welcome abord")
+
+
+greet2("Bijoy", "Das Gupta")
